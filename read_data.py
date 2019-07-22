9351654ac43f6eb73843654de6eeb25317c76b92
karan <kamu9767@colorado.edu>
2019-07-22 09:27:14 -0500

    read-data partially completed

--- read_data.py
+++ read_data.py
@@ -6,9 +6,35 @@ print("See examples (readme.txt); the correctness of input data is not "
       "extesively checked! All restriction are of type Ax + b (<=) 0 \n")
 read_file = input("Read data from a file? Y/N \n")
 
-u = np.arange(10)
-aaa = np.ndarray((2,2))
-print(aaa.ndim)
+"""looks like you can  not ask user to input array because they will be inputed as strings and thus saving it in file and loading might not work. For now lets write a alternate case of reading arrays form txt files"""
+# u = np.arange(10)
+# aaa = np.ndarray((2,2))
+# print(aaa.ndim)
+
+# a = np.array([[2,1],[2,1]])
+# b = np.array([2,2])
+# print(a.transpose(),b)
+# a = np.array(input("Enter the fucking 2d matrix \n"))
+# b = np.array(input("Enter the fucking 1d matrix \n"))
+# print(a.shape)
+# list = (a,'str',3)
+# tmpList = []
+# tmpList.append(np.asarray(a))
+# tmpList.append(b)
+# tmpList.append('str')
+# tmpList.append(4)
+# for i in list:
+#       print(type(i))
+# print("*********************")
+# for i in tmpList:
+#       print(type(i))
+# print(list[0].transpose())
+# print(a.ndim)
+# print("jkdksdkb " + str(b.ndim))
+# x,y = np.shape(a)
+# print(x)
+# print(y)
+
 
 DEBUG = True
 if read_file  == 'N' or read_file == 'n':
@@ -44,7 +70,7 @@ if read_file  == 'N' or read_file == 'n':
             y = np.array(input("Vector B"+str(i + 1)+ "("+str(np.size(A[i]))+ "x 1) for proposition"+ str(i + 1)+"\n"))
             B.append(y)
             #B.append(input("Vector B"+str(i + 1)+ "("+str(len(A[i]))+ "x 1) for proposition"+ str(i + 1)+"\n"))
-            print(A[i])
+            print(type(A[i]))
             #print(str(np.size(A[])))
             #B.append(input("Vector B" + str(i + 1) + "(" + str(A[,:].size) + "x 1) for proposition" + str(i + 1) + "\n"))
         #TODO add try and catch for accepting valid matrix types in A and for B
@@ -52,10 +78,12 @@ if read_file  == 'N' or read_file == 'n':
         A.append(np.array(input("Matrix (m" + str(N_p+1)+"x"+str(n)+") for state space boundaries \n" )))
         # print("A is fine")
         tmp = A.__getitem__(N_p)  # already a
-        print(tmp[0])
+        tmp1= A.__getitem__(0)
+        # print(tmp[0])
         print(type(tmp))
         print(tmp.reshape(1,1))
         print(tmp.ndim)
+        print(tmp1.ndim)
         #print(np.shape(np.asarray(tmp)))
         # a = np.array([2 2; 2 2])
         B.append(np.array(input("Vector (" + str(np.ndim(tmp)) + " X 1) for state space boundaries \n")))
@@ -75,3 +103,50 @@ if read_file  == 'N' or read_file == 'n':
         D_b = np.array(input("Column Vector D_b (" + str(n) + "x 1) : \n" ))
         U_A = np.array(input("Matrix U_A ( m_u x " + str(m) + ", m_u > " + str(m) +")defining bounded polytope containing speed boundaries: \n" ))
         U_b = np.array(input("Column Vector U_b ( " + str(np.size(U_A[:,1])) + "X 1) corresponding to matrix U_A: \n"))
+
+else:
+    # testList = []
+    # A = np.ndarray
+    # csv_fname = 'test.txt'
+    # with open(csv_fname) as fp:
+    #     for line in fp:
+    #         element = int(float(elt.strip()) for elt in line.split(','))
+    #         A.__add__(element)
+    #         testList.append(element)
+    #     print(testList)
+
+
+    #     fp.write("""\
+    # "A","B","C","D","E","F","timestamp"
+    # 611.88243,9089.5601,5133.0,864.07514,1715.37476,765.22777,1.291111964948E12
+    # 611.88243,9089.5601,5133.0,864.07514,1715.37476,765.22777,1.291113113366E12
+    # 611.88243,9089.5601,5133.0,864.07514,1715.37476,765.22777,1.291120650486E12
+    # """)
+
+
+
+    # Read the CSV file into a Numpy record array
+
+    #r = np.genfromtxt(csv_fname,float,'#',',')
+    # r = np.genfromtxt("test.csv",array,'#')
+
+    n = 2
+    N_p = 10
+    orig_alph = ['p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12'] #just check how many these do you need
+    alphabet = ['p01', 'p02', 'p03', 'p04', 'p05', 'p06', 'p07', 'p08', 'p09', 'p10', 'p11', 'p12'] #it will ave the same number as above with naming scheme changed
+    A = [np.array([0,1]),np.array([1,-1]),np.array([4,1]),np.array([4,-7]),np.array([-2,-1]),np.array([-1,-12]),np.array([-1,-1]),np.array([1,0]),np.array([0,1]),np.array([-6,4.5]),
+         np.array([[-1,0],[1,0],[0,-1],[0,1],[-3,-5],[1,-1],[-1,2.5],[-2,2.5]])]
+    B = [np.array([0]),np.array([0]),np.array([12]),np.array([34]),np.array([4]),np.array([31]),np.array([11]),np.array([-3]),np.array([-1.5]),np.array([-12]),np.array([[-5],[-7],[-3],[-6],[-15],[-7],[-15],[-17.5]])]
+    D_A = np.array([[0.2,-0.3],[0.5,-0.5]])
+    D_b = np.array([[0.5],[0.5]])
+    D_B = np.array([[1,0],[0,1]])
+    U_A = np.array([[0,2],[2,0],[0,-2],[-2,0]])
+    U_b = np.array([[0],[0],[0],[0]])
+
+print(A,"\n")
+print(B,"\n")
+print(D_B,"\n")
+print(D_A,"\n")
+print(D_b,"\n")
+print(U_A,"\n")
+print(U_b,"\n")
