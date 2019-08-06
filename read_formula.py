438d74125849320cfd2f8ecb6bcbe8c640592ddc
youngkaran24 <kamu9767@colorado.edu>
2019-08-06 10:00:18 -0500

    Added read_fomula which replaces the p1 with p01 and so on

---
+++ read_formula.py
@@ -0,0 +1,14 @@
+from read_data import ReadData
+import numpy as np
+import re
+
+alphabet = ReadData.alphabet
+orig_alph = ReadData.orig_alph
+
+class Read_formula:
+    N_p = len(alphabet) - 1
+    formula = input("LTL_x (without next operator) formula (between apostrophes) involving all" + str(N_p) + "atomic propositions:\n")
+    rev_Np = np.arange(N_p,-1,-1)
+    for i in rev_Np:
+        formula = re.sub(orig_alph[i],alphabet[i],formula)
+    print(formula)
