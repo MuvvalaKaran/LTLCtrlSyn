Wrote the new code for the create Buchi for full description. Our Buchi Automaton matches but T_adj doesn't. Got the matlab code to be working and the formula was faulty to begin with. Fixed the predicate issue as well and minor fixes here and there

src/invalidate_transitions.py
@@ -16,7 +16,7 @@ class Invalid_Transition:
     prec = pow(10,5)*np.finfo(float).eps
     updated_Tp_adj = trans_sys.Tp_adj
     rank_method = True  # this is faster but more restrictive method that eliminates lots of possible transitions
-    linprog_method = True  # this method is slightly slower but has more spurious transitions
+    linprog_method = False  # this method is slightly slower but has more spurious transitions
     # a mix and match also gives a mixed result but seems to be the closest one.
 
     for i in range(0,len(trans_sys.Tp.get("Tp.Q"))):

src/read_formula.py
@@ -12,9 +12,10 @@ class Read_formula:
     N_p = len(alphabet) - 1
     formula = input("LTL_x (without next operator) formula (between apostrophes) involving all " + str(N_p) + "atomic propositions:\n")
     if(read_formula):
-        formula = '(F((p3 & p10) & F((!p4 & p5 & p6 & p8) & F(!p1 & !p6 & !p8))) & !(p4 | p7 |(p1 & !p2 & !p5 & p9)))'
+        # formula = '(F((p3 & p10) & F((!p4 & p5 & p6 & p8) & F(!p1 & !p6 & !p8))) & !(p4 | p7 |(p1 & !p2 & !p5 & p9)))'
         # formula = "F(p1 & p2 & p3 & p4 & p5 & p6 & p7 & p8 & p9 & p10)"
         # formula = '(F((p3 & p10)))'
+        formula = 'G(F((p3 & p10) & F((!p4 & p5 & p6 & p8) & F(!p1 & !p6 & p8))) & !(p4 | p7 |(p1 & !p2 & !p5 & p9)))'
     rev_Np = np.arange(N_p,-1,-1)
     for i in rev_Np:
         formula = re.sub(orig_alph[i],alphabet[i],formula)